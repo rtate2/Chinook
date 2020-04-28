e56584d932cd0de0e65010b234053966d17ec335
Randy Tate <randytatejr@gmail.com>
2020-04-28 08:53:57 -0500

    completed 1-4 queries

---
+++ non_usa_customers.sql
@@ -0,0 +1,5 @@
+--Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+
+Select FirstName, LastName, CustomerId, Country
+FROM Customer
+WHERE Country != 'USA'
